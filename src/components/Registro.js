2dc0f79b50d3818ffa4908e8aa5364ed018376e2
AngeloMoron05 <angelofabianmoron05@gmail.com>
2024-04-09 16:57:30 -0500

    Agregando validaciones registro

--- src/components/Registro.js
+++ src/components/Registro.js
@@ -5,7 +5,7 @@ import './registro/registro.css'
 import { useState } from 'react';
 
 export default function Registro() {
-  const [values, setValues] = useState()({
+  const [values, setValues] = useState({
     id: "",
     names: "",
     lastname: "",
@@ -17,6 +17,46 @@ export default function Registro() {
     vrfpassword: ""
   })
 
+  function func_errorId(){
+    seterrorID(false);
+  }
+  function func_errorNames(){
+    seterrorNames(false);
+  }
+  function func_errorLastName(){
+    seterrorLastName(false);
+  }
+  function func_errorEmail(){
+    seterrorEmail(false);
+  }
+  function func_errorAdress(){
+    seterrorAdress(false);
+  }
+  function func_errorPhone(){
+    seterrorPhone(false);
+  }
+  function func_errorBornDate(){
+    seterrorBornDate(false);
+  }
+  function func_errorPassword(){
+    seterrorPassword(false);
+  }
+  function func_errorVrfPassword(){
+    seterrorVrfPassword(false);
+    seterrorVrfPasswordEmpty(false);
+  }
+
+  const [errorID, seterrorID] = useState(false);
+  const [errorNames, seterrorNames] = useState(false);
+  const [errorLastName, seterrorLastName] = useState(false);
+  const [errorEmail, seterrorEmail] = useState(false);
+  const [errorAdress, seterrorAdress] = useState(false);
+  const [errorPhone, seterrorPhone] = useState(false);
+  const [errorBornDate, seterrorBornDate] = useState(false);
+  const [errorPassword, seterrorPassword] = useState(false);
+  const [errorVrfPassword, seterrorVrfPassword] = useState(false);
+  const [errorVrfPasswordEmpty, seterrorVrfPasswordEmpty] = useState(false);
+
   const handleChange = (e) => {
     const {name, value} = e.target;
     const newValues = {
@@ -26,63 +66,118 @@ export default function Registro() {
     setValues(newValues);
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const vefEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const vefPassword = /^(?=.*[A-Z])(?=(?:.*\d){2,}).{9,}$/;
+
+    if(values.id.length < 5 || values.id.length > 10 || values.id.length==0) {
+      seterrorID(true);
+      return;
+    }else if(values.names.length < 3 || values.names.length==0){
+      seterrorNames(true);
+      return;
+    }else if(values.lastname.length < 3 || values.lastname.length==0){
+      seterrorLastName(true);
+      return;
+    }else if(values.email.length==0){
+      seterrorEmail(true);
+      return;
+    }else if(!vefEmail.test(values.email)){
+      seterrorEmail(true);
+      return;
+    }else if(values.adress.length==0){
+      seterrorAdress(true);
+      return;
+    }else if(values.phone.length < 10 || values.phone.length==0){
+      seterrorPhone(true);
+      return;
+    }else if(values.borndate.length==0){
+      seterrorBornDate(true);
+      return;
+    }else if(values.password.length == 0){
+      seterrorPassword(true);
+      return;
+    }else if(!vefPassword.test(values.password)){
+      seterrorPassword(true);
+      return;
+    }else if(values.vrfpassword.length == 0){
+      seterrorVrfPasswordEmpty(true);
+      return;
+    }else if(values.vrfpassword!=values.password){
+      seterrorVrfPassword(true);
+    }
+  }
+
+
   return (
     <div>
       <Header/>
       <div className='bigcont'>
         <div className='cont'>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div class="form-group">
               <label for="exampleInputEmail1">Identificacion</label>
-              <input id="id" type="number" class="form-control" placeholder="1234567890" name='id' onChange={handleChange}></input>
+              <input id="id" type="number" class="form-control" placeholder="1234567890" name='id' onChange={handleChange} onClick={func_errorId}></input>
+              {errorID ? <p className='error'>La identificacion debe tener de 4 a 10 caracteres.</p> : ""}
             </div>
             <br></br>
 
             <div class="form-group">
               <label for="exampleInputPassword1">Nombre</label>
-              <input id="names" type="text" class="form-control" placeholder="Juan" name='names' onChange={handleChange}></input>
+              <input id="names" type="text" class="form-control" placeholder="Juan" name='names' onChange={handleChange} onClick={func_errorNames}></input>
+              {errorNames ? <p className='error'>El nombre debe tener 3 o mas caracteres.</p> : ""}
             </div>
             <br></br>
 
             <div class="form-group">
               <label for="exampleInputEmail1">Apellido</label>
-              <input id="lastname" type="text" class="form-control" placeholder="Gomez" name='lastname' onChange={handleChange}></input>
+              <input id="lastname" type="text" class="form-control" placeholder="Gomez" name='lastname' onChange={handleChange} onClick={func_errorLastName}></input>
+              {errorLastName ? <p className='error'>El apellido debe tener 3 o mas caracteres.</p> : ""}
             </div>
             <br></br>
 
             <div class="form-group">
               <label for="exampleInputEmail1">Correo Electronico</label>
-              <input id="email" type="email" class="form-control" aria-describedby="emailHelp" placeholder="ejemplo@mail.com" name='email' onChange={handleChange}></input>
+              <input id="email" type="email" class="form-control" aria-describedby="emailHelp" placeholder="ejemplo@mail.com" name='email' onChange={handleChange} onClick={func_errorEmail}></input>
+              {errorEmail ? <p className='error'>Debe ingresar una direccion de email valida.</p> : ""}
             </div>
             <br></br>
 
             <div class="form-group">
               <label for="exampleInputEmail1">Direccion</label>
-              <input id="adress" type="text" class="form-control" placeholder="Dosquebradas, Pereira, Colombia" name='adress' onChange={handleChange}></input>
+              <input id="adress" type="text" class="form-control" placeholder="Dosquebradas, Pereira, Colombia" name='adress' onChange={handleChange} onClick={func_errorAdress}></input>
+              {errorAdress ? <p className='error'>Debe ingresar su direccion.</p> : ""}
             </div>
             <br></br>
 
             <div class="form-group">
               <label for="exampleInputEmail1">Telefono</label>
-              <input id="phone" type="number" class="form-control" placeholder="310 000 0000" name='phone' onChange={handleChange}></input>
+              <input id="phone" type="number" class="form-control" placeholder="310 000 0000" name='phone' onChange={handleChange} onClick={func_errorPhone}></input>
+              {errorPhone ? <p className='error'>Debe ingresar un numero telefonico valido.</p> : ""}
             </div>
             <br></br>
 
             <div class="form-group">
               <label for="exampleInputEmail1">Fecha Nacimiento</label>
-              <input id="borndate" type="date" class="form-control" name='borndate' onChange={handleChange}></input>
+              <input id="borndate" type="date" class="form-control" name='borndate' onChange={handleChange} onClick={func_errorBornDate}></input>
+              {errorBornDate ? <p className='error'>Debe ingresar su fecha de nacimiento.</p> : ""}
             </div>
             <br></br>
 
             <div class="form-group">
               <label for="exampleInputEmail1">Contraseña</label>
-              <input id="password" type="password" class="form-control" name='password' onChange={handleChange}></input>
+              <input id="password" type="password" class="form-control" name='password' onChange={handleChange} onClick={func_errorPassword}></input>
+              {errorPassword ? <p className='error'>Ingrese una contraseña con al menos una letra mayuscula y dos caracteres numericos.</p> : ""}
             </div>
             <br></br>
 
             <div class="form-group">
               <label for="exampleInputEmail1">Repita su contraseña</label>
-              <input id="vrfpassword" type="password" class="form-control" name='vrfpassword' onChange={handleChange}></input>
+              <input id="vrfpassword" type="password" class="form-control" name='vrfpassword' onChange={handleChange} onClick={func_errorVrfPassword}></input>
+              {errorVrfPasswordEmpty ? <p className='error'>Debe ingresar de nuevo la contraseña.</p> : ""}
+              {errorVrfPassword ? <p className='error'>Las contraseñas no coinciden.</p> : ""}
             </div>
             <br></br>
 
