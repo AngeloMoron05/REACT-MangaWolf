3ca797b083a33ce5954eb5cd6037cf2b52eae8fe
AngeloMoron05 <angelofabianmoron05@gmail.com>
2024-06-19 17:17:00 -0500

    Volviendo a agregar el id al boton

--- src/components/carrito/CarritoElements.js
+++ src/components/carrito/CarritoElements.js
@@ -44,7 +44,7 @@ function CarritoElements() {
                                             <h5 className="mb-0">$ {formatoNumero(libs.precioCarrito)}</h5>
                                         </div>
                                         <div className="col-md-1 col-lg-1 col-xl-1 text-end">
-                                            <button className="btn btn-danger" onClick={eliminarLibroDelCarrito}><i className="bi bi-trash3-fill"></i></button>
+                                            <button className="btn btn-danger" id={libs.id} onClick={eliminarLibroDelCarrito}><i className="bi bi-trash3-fill"></i></button>
                                         </div>
                                     </div>
                                 </div>
